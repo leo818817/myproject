b407c13b1e56509ca4b68c8eb55a2ff3a1bc2868
leo818817 <leo818817@gmail.com>
2019-02-04 20:11:26 +0800

    Java

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+        Person person = new Person();
+        person.hello();
+        int age = 19;
+    }
+}
---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,9 @@
+package com.tom;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello world");
+
+    }
+
+}

f041e03b9c8e8662190c0d4bc1eb85b70907a049
leo818817 <leo818817@gmail.com>
2019-02-04 20:08:26 +0800

    1

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,17 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+    val  age : Int=19
+    var name : String
+    name = "Hank"
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
